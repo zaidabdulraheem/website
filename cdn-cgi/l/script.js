e34a61809d2b8e215768cf4eac21d77358f5ea0a
Zaid abdul raheem <zaid98508@gmail.com>
2024-06-29 14:49:44 +0530

    Delete cdn-cgi directory

--- cdn-cgi/l/script.js
+++
@@ -1,5 +0,0 @@
-const email = "youremail@gmail.com"; // Replace with your Gmail address
-const emailElement = document.getElementById("email");
-
-// Display the email address
-emailElement.textContent = email;
--- cdn-cgi/script.js
+++
@@ -1,5 +0,0 @@
-const email = "mdomer19967@gmail.com"; // Replace with your Gmail address
-const emailElement = document.getElementById("email");
-
-// Display the email address
-emailElement.textContent = email;
